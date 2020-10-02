affd23c3ec4b109e8da70ba5fbd1524fda32564f
thomashoang <tphoang@umich.edu>
2020-10-01 22:08:04 -0700

    image upload to cloudinary

--- src/components/AddProject/AddProject.js
+++ src/components/AddProject/AddProject.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { withFirebase } from '../Firebase';
 import { classes } from './AddProject.css';
 import { makeStyles } from '@material-ui/core/styles';
+import { CloudinaryContext } from "cloudinary-react";
+import { openUploadWidget } from '../../utils/CloudinaryService';
 
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
@@ -35,40 +37,66 @@ const AddProject = (props) => {
         }).catch(err => console.error(err));
     }
 
+    const beginUpload = (event) => {
+        event.preventDefault();
+        const uploadOptions = {
+            cloudName: "dpzfuel4y",
+            tags: ['image', 'anImage'],
+            uploadPreset: "lt0svpvk"
+        };
+        openUploadWidget(uploadOptions, (error, photos) => {
+            if (!error) {
+                console.log(photos);
+            } else {
+                console.log(error);
+            }
+        })
+        //path to url event:queues-end -> data -> info -> files -> 'the file object' -> upload info -> url
+    };
+
     const classes = useStyles();
     let isDisabled = !(name && category);
     return (
-        <div className="add-project__container">
-            <div className="add-project__title">
-                Create Project
+        <CloudinaryContext cloudName="dpzfuel4y">
+            <div className="add-project__container">
+                <div className="add-project__title">
+                    Create Project
             </div>
-            <form className={classes.root + ' add-project__form'}>
-                <TextField
-                    type="text"
-                    id="name"
-                    label="Name"
-                    variant="outlined"
-                    size="small"
-                    onChange={nameChangedHandler}
-                />
-                <TextField
-                    type="text"
-                    id="category"
-                    label="Category"
-                    variant="outlined"
-                    size="small"
-                    onChange={categoryChangedHandler}
-                />
-                <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={handleCreateProject}
-                    disabled={isDisabled}
-                >
-                    Create
+                <form className={classes.root + ' add-project__form'}>
+                    <TextField
+                        type="text"
+                        id="name"
+                        label="Name"
+                        variant="outlined"
+                        size="small"
+                        onChange={nameChangedHandler}
+                    />
+                    <TextField
+                        type="text"
+                        id="category"
+                        label="Category"
+                        variant="outlined"
+                        size="small"
+                        onChange={categoryChangedHandler}
+                    />
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={handleCreateProject}
+                        disabled={isDisabled}
+                    >
+                        Create
                 </Button>
-            </form>
-        </div>
+                    <button
+                        variant="contained"
+                        color="primary"
+                        onClick={beginUpload}
+                    >
+                        Upload Image
+                </button>
+                </form>
+            </div>
+        </CloudinaryContext>
     );
 }
 
