9e30f194fe355890aee10b046f6977efe8170dc8
thomashoang <tphoang@umich.edu>
2020-10-13 22:03:12 -0700

    Add settings icon and setting options to proj card

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
+import SettingsIcon from '@material-ui/icons/Settings';
+import Button from '@material-ui/core/Button';
 import './ProjectCard.css';
 
 const useStyles = makeStyles({
@@ -13,33 +15,86 @@ const useStyles = makeStyles({
         transform: 'translateX(-50%)',
         width: 300
     },
+    button: {
+        '& .MuiButton-root': {
+            margin: '5px'
+        }
+    },
     media: {
         margin: '18px 18px 0 18px',
         width: 266,
         height: 200
     },
     content: {
-        textAlign: 'center',
-        fontSize: 18
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        fontSize: 18,
+        '&:last-child': {
+            paddingBottom: '12px'
+        }
     }
 });
 
 
 const ProjectCard = (props) => {
     const classes = useStyles();
+    const [showSettings, setShowSettings] = useState(false);
 
-    return (
-        <Card className={classes.root} onClick={props.onClick}>
-            <CardActionArea>
+    const handleShowSettings = () => {
+        setShowSettings(!showSettings);
+    };
+
+    const projectCard = (
+        <div>
+            <CardActionArea onClick={props.onClick}>
                 <CardMedia
                     className={classes.media}
                     image={props.projectImage}
                     title="Contemplative Marlo"
                 />
-                <CardContent className={classes.content}>
-                    {props.projectName}
-                </CardContent>
             </CardActionArea>
+            <CardContent className={classes.content}>
+                <div className='projectCardName'>
+                    {props.projectName}
+                </div>
+                <div className='projectCardSetting'>
+                    <SettingsIcon onClick={handleShowSettings} />
+                </div>
+            </CardContent>
+        </div>
+    );
+
+    const settingsCard = (
+        <div>
+            <div className={classes.button + ' projectCardSettings'}>
+                <Button
+                    variant="contained"
+                    color="primary"
+                >
+                    Edit
+                </Button>
+                <Button
+                    variant="contained"
+                    color="secondary"
+                >
+                    Delete
+                </Button>
+            </div>
+            <CardContent className={classes.content}>
+                <div className='projectCardName'>
+                    {props.projectName}
+                </div>
+                <div className='projectCardSetting'>
+                    <SettingsIcon onClick={handleShowSettings} />
+                </div>
+            </CardContent>
+        </div>
+    );
+
+    return (
+        <Card className={classes.root}>
+            {showSettings ? settingsCard : projectCard}
         </Card>
     );
 };
